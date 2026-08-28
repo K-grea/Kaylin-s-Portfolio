037b671a08de61d7e18716ee14485ca0ba34870d
Kaylin Greaves <kgreaves@my.campus.edu>
2026-08-28 14:07:18 -0500

    Fix button ID for favorite things toggle in about.html and update script.js for consistency

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ console.log("JavaScript is working!");
 const favoriteButton = document.querySelector("#favoriteButton");
 const favoriteList = document.querySelector(".favorite-list");
 
+console.log(favoriteButton);
+console.log(favoriteList);
+
 favoriteButton.addEventListener("click", function () {
     if (favoriteList.style.display === "none") {
         favoriteList.style.display = "block";
